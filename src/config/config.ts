d87299eb450564ffbe5d244af6be3a8698a23812
Chris <chris.fraanc@gmail.com>
2024-06-22 22:42:44 +0200

    [CONFIG] Adding DB_URL in env, setting the tsconfig module to es2022

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
-import getenv from "getenv";
+import getenv from 'getenv';
 
 export const config = getenv.multi({
-  NODE_ENV: ["NODE_ENV", "development"],
-  PORT: ["PORT", 3000, "int"],
+  NODE_ENV: ['NODE_ENV', 'development'],
+  PORT: ['PORT', 3000, 'int'],
+  DB_URL: ['DB_URL', 'string'],
 });
