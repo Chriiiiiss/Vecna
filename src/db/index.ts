711d10ce9af58bab37ad3260ea4c387d58f2eb71
Chris <chris.fraanc@gmail.com>
2024-06-24 03:02:40 +0200

    [MOD] Adding schema to the drizzle db client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,25 @@
 import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { config } from '../config/config';
+import * as characterSchema from './schemas/characters';
+import * as classSchema from './schemas/class';
+import * as raceSchema from './schemas/races';
+import * as userSchema from './schemas/users';
+import * as traitSchema from './schemas/traits';
+import * as spellSchema from './schemas/spells';
+import * as spellToClassSchema from './schemas/spellsToClass';
 
-const client = postgres(config.DB_URL!);
-export const db = drizzle(client);
+const connectionString = config.DB_URL;
+
+const client = postgres(connectionString);
+export const db = drizzle(client, {
+  schema: {
+    ...characterSchema,
+    ...classSchema,
+    ...raceSchema,
+    ...userSchema,
+    ...traitSchema,
+    ...spellSchema,
+    ...spellToClassSchema,
+  },
+});
