b154bd0d9556f8c8259d59e02fd26981b5cc2387
Chris <chris.fraanc@gmail.com>
2024-06-24 02:34:24 +0200

    [SETUP] Adding dotenv toi make getenv retrieve the good env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
 import getenv from 'getenv';
+import 'dotenv/config';
 
 export const config = getenv.multi({
-  NODE_ENV: ['NODE_ENV', 'development'],
+  NODE_ENV: ['NODE_ENV', 'development', 'string'],
   PORT: ['PORT', 3000, 'int'],
-  DB_URL: ['DB_URL', 'string'],
+  DB_URL: ['DB_URL', 'xxx', 'string'],
 });
